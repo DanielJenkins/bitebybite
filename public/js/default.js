a2112f315c230e728e55aea4290f81235413a9f0
Daniel Jenkins <DanielPatrickJenkins@gmail.com>
2015-12-16 15:53:25 -0800

    Adds lng/lat calculations for a desired number of points between origin and destination - #1

--- public/js/default.js
+++ public/js/default.js
@@ -1,10 +1,13 @@
-function init() {
-  var originElement = document.getElementById('origin');
-  var destinationElement = document.getElementById('destination');
-  var originAutocomplete = new google.maps.places.Autocomplete(originElement);
-  var destinationAutocomplete = new google.maps.places.Autocomplete(destinationElement);
-}
+var originElement = document.getElementById('origin');
+var destinationElement = document.getElementById('destination');
+var originAutocomplete;
+var destinationAutocomplete;
 
+
+function init() {
+  originAutocomplete = new google.maps.places.Autocomplete(originElement);
+  destinationAutocomplete = new google.maps.places.Autocomplete(destinationElement);
+}
 google.maps.event.addDomListener(window, 'load', init);
 
 var categoryElement = document.getElementById('category');
@@ -12,7 +15,40 @@ var categoryElement = document.getElementById('category');
 var search = document.getElementById('search');
 search.addEventListener('click',runSearch,false);
 
-function runSearch() {
-  var origin = originAutocomplete.getPlace();
-  var destination = destinationAutocomplete.getPlace();
+function changeView() {
+  ////Change visibility of parts of the page
 }
+
+var origin;
+var originLat;
+var originLng;
+var originLatLng;
+var destination;
+var destinationLat;
+var destinationLng;
+var destinationLatLng;
+var searchPlaces = [];
+function runSearch(e) {
+  e.preventDefault();
+  //Get origin and destination lat&lng
+  origin = originAutocomplete.getPlace();
+  originLat = origin.geometry.location.lat();
+  originLng = origin.geometry.location.lng();
+  originLatLng = new google.maps.LatLng({lat: originLat, lng: originLng}); 
+  destination = destinationAutocomplete.getPlace();
+  destinationLat = destination.geometry.location.lat();
+  destinationLng = destination.geometry.location.lng();
+  destinationLatLng = new google.maps.LatLng({lat: destinationLat, lng: destinationLng}); 
+
+  var numSearches = 4; //Starts at 0
+  var LatLngObj;
+  searchPlaces[0] = originLatLng;
+  for (var i = 0; i <= numSearches; i++) {
+    LatLngObj = google.maps.geometry.spherical.interpolate(originLatLng, destinationLatLng, (i/numSearches));
+    searchPlaces[i] = {lat: LatLngObj.lat(), lng: LatLngObj.lng()}
+    console.log(searchPlaces[i]);
+  };
+}
+
+
+
