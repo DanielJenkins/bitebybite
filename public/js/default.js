e8a40874b6e0624df440ecedc0aac3ade2041911
Daniel Jenkins <DanielPatrickJenkins@gmail.com>
2015-12-16 10:19:19 -0800

    Adds Autocomplete functionality to location searches

--- public/js/default.js
+++ public/js/default.js
@@ -1,11 +1,18 @@
 function init() {
-  var originElement = getElementById('origin');
-  var destinationElement = getElementById('destination');
+  var originElement = document.getElementById('origin');
+  var destinationElement = document.getElementById('destination');
   var originAutocomplete = new google.maps.places.Autocomplete(originElement);
   var destinationAutocomplete = new google.maps.places.Autocomplete(destinationElement);
 }
 
 google.maps.event.addDomListener(window, 'load', init);
 
-var search getElementById('search');
+var categoryElement = document.getElementById('category');
 
+var search = document.getElementById('search');
+search.addEventListener('click',runSearch,false);
+
+function runSearch() {
+  var origin = originAutocomplete.getPlace();
+  var destination = destinationAutocomplete.getPlace();
+}
