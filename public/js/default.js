6aade3c0e673cd7e4293098a2018440e15b45413
Daniel Jenkins <DanielPatrickJenkins@gmail.com>
2015-12-15 16:30:02 -0800

    Beginning to add autocomplete functionality

--- public/js/default.js
+++ public/js/default.js
@@ -1,3 +1,11 @@
-var origin = getElementById('origin');
-var destination
-var submit getElementById('submit');
+function init() {
+  var originElement = getElementById('origin');
+  var destinationElement = getElementById('destination');
+  var originAutocomplete = new google.maps.places.Autocomplete(originElement);
+  var destinationAutocomplete = new google.maps.places.Autocomplete(destinationElement);
+}
+
+google.maps.event.addDomListener(window, 'load', init);
+
+var search getElementById('search');
+
