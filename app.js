b15aad1e635c40dc117c1f33d0b4a010672b003a
Daniel Jenkins <DanielPatrickJenkins@gmail.com>
2016-01-29 11:06:24 -0800

    Updating for Heroku

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 var yelpRouter = require('./yelp.js');
 
-app.use(express.static('application/public'));
+app.use(express.static('./public'));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
